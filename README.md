bd767134462c3de2cc337222177bb9eec980ec8a
excaliburXD <eexcalibur32@gmail.com>
2024-04-21 03:55:12 +0700

    skeleton tree

---
+++ README.md
@@ -0,0 +1,5 @@
+# TWRP Device tree for INFINIX NOTE 10 PRO (X695C/X695)
+
+* Status: Test works (buildable & bootable)
+* Credits: gilanggegea
+
---
+++ twrp_Infinix_X695C.mk
@@ -0,0 +1,39 @@
+#
+# Copyright (C) 2020 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Release name
+PRODUCT_RELEASE_NAME := Infinix_X695C
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from Infinix-X695C device
+$(call inherit-product, device/infinix/Infinix_X695C/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := Infinix_X695C
+PRODUCT_NAME := twrp_Infinix_X695C
+PRODUCT_BRAND := Infinix
+PRODUCT_MODEL := Infinix X695C
+PRODUCT_MANUFACTURER := INFINIX MOBILITY LIMITED
